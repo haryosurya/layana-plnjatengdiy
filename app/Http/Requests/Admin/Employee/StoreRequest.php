8ecba5be9a4e524e52e1a183ccb51523c24eb86d
haryosy <55373600+haryosurya@users.noreply.github.com>
2022-10-05 11:21:19 +0700

    api

--- app/Http/Requests/Admin/Employee/StoreRequest.php
+++ app/Http/Requests/Admin/Employee/StoreRequest.php
@@ -29,35 +29,14 @@ class StoreRequest extends CoreRequest
             'employee_id' => 'required|unique:employee_details|max:100',
             'name' => 'required|max:50',
             'email' => 'required|email:rfc|unique:users|max:100',
-            'password' => 'required|min:8|max:50',
-            'slack_username' => 'nullable|unique:employee_details,slack_username|max:30',
-            'hourly_rate' => 'nullable|numeric',
+            'password' => 'required|min:8|max:50', 
             'joining_date' => 'required',
             'last_date' => 'nullable|date_format:"' . $setting->date_format . '"|after_or_equal:joining_date',
             'date_of_birth' => 'nullable|date_format:"' . $setting->date_format . '"|before_or_equal:'.now($setting->timezone)->toDateString(),
             'department' => 'required',
-            'designation' => 'required',
-            'country' => 'required_with:mobile',
+            'designation' => 'required', 
         ];
-
-        if (request()->telegram_user_id) {
-            $rules['telegram_user_id'] = 'nullable|unique:users,telegram_user_id';
-        }
-
-        if (request()->get('custom_fields_data')) {
-            $fields = request()->get('custom_fields_data');
-
-            foreach ($fields as $key => $value) {
-                $idarray = explode('_', $key);
-                $id = end($idarray);
-                $customField = \App\Models\CustomField::findOrFail($id);
-
-                if ($customField->required == 'yes' && (is_null($value) || $value == '')) {
-                    $rules['custom_fields_data['.$key.']'] = 'required';
-                }
-            }
-
-        }
+ 
 
         return $rules;
     }
@@ -65,20 +44,7 @@ class StoreRequest extends CoreRequest
     public function attributes()
     {
         $attributes = [];
-
-        if (request()->get('custom_fields_data')) {
-            $fields = request()->get('custom_fields_data');
-
-            foreach ($fields as $key => $value) {
-                $idarray = explode('_', $key);
-                $id = end($idarray);
-                $customField = \App\Models\CustomField::findOrFail($id);
-
-                if ($customField->required == 'yes') {
-                    $attributes['custom_fields_data['.$key.']'] = $customField->label;
-                }
-            }
-        }
+ 
 
         return $attributes;
     }
