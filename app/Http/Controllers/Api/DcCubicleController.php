a614513a841a88536c241156c6091c48234ee951
haryosy <55373600+haryosurya@users.noreply.github.com>
2022-10-11 13:35:16 +0700

    api

--- app/Http/Controllers/Api/DcCubicleController.php
+++ app/Http/Controllers/Api/DcCubicleController.php
@@ -101,9 +101,9 @@ class DcCubicleController extends Controller
                 $lr = '';
             }
             $gi = Dc_incoming_feeder::where('INCOMING_ID',$result['INCOMING_ID'])->firstorFail();
-            $history_pd = ews_inspeksi_pd::where('id_outgoing',$id) ->orderBy('id_inspeksi_pd','DESC')->firstorFail(); 
-            $history_pmt = Sm_meter_gi::where('OUTGOING_ID',$id)->orderBy('OUTGOING_METER_ID','DESC')->firstorFail();
-            $history_asset = ews_inspeksi_aset::where('id_outgoing',$id)->orderBy('id_inspeksi_aset','DESC')->firstorFail();
+            $history_pd = ews_inspeksi_pd::where('id_outgoing',$id)->limit(1) ->orderBy('id_inspeksi_pd','DESC')->get(); 
+            $history_pmt = Sm_meter_gi::where('OUTGOING_ID',$id)->limit(1) ->orderBy('OUTGOING_METER_ID','DESC')->get();
+            $history_asset = ews_inspeksi_aset::where('id_outgoing',$id)->limit(1) ->orderBy('id_inspeksi_aset','DESC')->get();
             return response()->json(array(    
                 'status'=>true,  
                 'data' => array (
